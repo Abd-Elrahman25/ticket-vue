3771f3fceebce5e9b971a4f5117d7c0587036d0f
Marwan Muhammad <marwanmohammad2013@gmail.com>
2024-04-15 20:19:49 +0200

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import TicketView from "../views/TicketView.vue";
 
 const routes = [
   {
@@ -8,18 +10,22 @@ const routes = [
     component: HomeView,
   },
   {
+    path: "/ticket",
+    name: "ticket",
+    component: TicketView,
+  },
+  {
     path: "/about",
     name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
---
+++ src/views/TicketView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>{{ backendData }}</div>
+
+  <a href="#" @click="testFun">Get Data</a>
+  <DataTable
+    class="table clawback-refund-table table-bordered w-100 nowrap display"
+    id="table-orders"
+    ref="table"
+    :data="data"
+  >
+    <thead>
+      <tr>
+        <th>A</th>
+        <th>B</th>
+      </tr>
+    </thead>
+  </DataTable>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+import DataTable from "datatables.net-vue3";
+import Select from "datatables.net-select";
+
+DataTable.use(Select);
+export default {
+  components: { DataTable },
+  name: "TicketView",
+  data() {
+    return {
+      tickets: [],
+      data: [
+        [1, 2],
+        [3, 4],
+      ],
+      backendData: "",
+    };
+  },
+  mounted() {},
+  methods: {
+    testFun() {
+      axios
+        .get("http://127.0.0.1:8000/get-tickets")
+        .then((response) => {
+          // Handle successful response
+          if (response) {
+            console.log(response.data); // Do something with the received data
+            this.backendData = JSON.stringify(response.data.tickets);
+          }
+        })
+        .catch((error) => {
+          // Handle error
+          console.error("Error fetching data:", error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+@import "datatables.net-dt";
+</style>
